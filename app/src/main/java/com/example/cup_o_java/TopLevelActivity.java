485aaef5f21011184528236c5437acfc4bcb6b08
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-15 22:34:18 -0400

    Replaced old adapterview listener with new lambda

--- app/src/main/java/com/example/cup_o_java/TopLevelActivity.java
+++ app/src/main/java/com/example/cup_o_java/TopLevelActivity.java
@@ -3,7 +3,6 @@ package com.example.cup_o_java;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -15,13 +14,10 @@ public class TopLevelActivity extends Activity {
         setContentView(R.layout.activity_top_level);
 
         //Create an onItemClickListener
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
-                if (position == 0) {
-                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
-                    startActivity(intent);
-                }
+        AdapterView.OnItemClickListener itemClickListener = (listView, view, position, id) -> {
+            if (position == 0) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                startActivity(intent);
             }
         };
         //Add the listener to the list view
