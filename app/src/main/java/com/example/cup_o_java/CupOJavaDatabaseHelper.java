b50017d6293cc84447e6ff6b4acd718c32e98852
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-16 11:18:25 -0400

    create boilerplate for SQLite integration

---
+++ app/src/main/java/com/example/cup_o_java/CupOJavaDatabaseHelper.java
@@ -0,0 +1,22 @@
+package com.example.cup_o_java;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class CupOJavaDatabaseHelper extends SQLiteOpenHelper {
+
+    CupOJavaDatabaseHelper(Context context) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
