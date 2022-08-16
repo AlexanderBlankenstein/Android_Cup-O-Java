80d25ff6db532643d711c00455159f2b26621e92
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-16 13:09:06 -0400

    tidy up code

--- app/src/main/java/com/example/cup_o_java/TopLevelActivity.java
+++ app/src/main/java/com/example/cup_o_java/TopLevelActivity.java
@@ -3,6 +3,7 @@ package com.example.cup_o_java;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -12,14 +13,18 @@ public class TopLevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        setupOptionsListView();
+    }
 
-        //Create an onItemClickListener
+    private void setupOptionsListView() {
+        //Create an OnItemClickListener
         AdapterView.OnItemClickListener itemClickListener = (listView, view, position, id) -> {
             if (position == 0) {
                 Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
                 startActivity(intent);
             }
         };
+
         //Add the listener to the list view
         ListView listView = findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
