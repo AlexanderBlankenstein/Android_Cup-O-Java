48fc2214d1863c9ecd9e233e1b43d7a51774ccc5
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-16 12:36:50 -0400

    updated DrinkCategoryActivity code to now open and read data from SQLite db

--- app/src/main/java/com/example/cup_o_java/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/cup_o_java/DrinkCategoryActivity.java
@@ -2,25 +2,42 @@ package com.example.cup_o_java;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks
-        );
         ListView listDrinks = findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
+        SQLiteOpenHelper cupojavaDatabaseHelper = new CupOJavaDatabaseHelper(this);
+        try {
+            db = cupojavaDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK", new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        } catch (SQLException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
         //Create the listener
         AdapterView.OnItemClickListener itemClickListener = (listDrink, itemView, position, id) -> {
@@ -32,5 +49,13 @@ public class DrinkCategoryActivity extends Activity {
 
         //Assign the listener to the list view
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    //Keep the cursor open until the adapter no longer needs it (when it gets destroyed)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
